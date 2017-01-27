6469b8b0e7757660020bd6583ec5c4410fd0ddfe
Maciej Galka <maciejgalka@MacBook-Pro-Maciej.local>
2017-01-27 19:10:30 +0100

    checkboxes styles

--- form.js
+++ form.js
@@ -2,6 +2,6 @@ $(document).ready(function(){
     $('#form').fadeIn(2000);
 
     $('li input').hover(function () {
-        $('li input').toggleClass("focus")
+        $(this).toggleClass("focus")
     });
 });
